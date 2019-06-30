a995d1fc18aee033d13ed1e819e1c0f604ee6c7b
peibinchen <1198960858@qq.com>
2019-06-30 16:26:51 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#rlaunch --cpu=2 --gpu=1 --memory=10000 -- python3 -m semi_supervised.experiments.tempens.cifar10_test &
-#rlaunch --cpu=2 --gpu=1 --memory=10000 -- python3 -m semi_supervised.experiments.pi.cifar10_test &
-#rlaunch --cpu=2 --gpu=1 --memory=10000 -- python3 -m semi_supervised.experiments.mean_teacher.cifar10_test &
-rlaunch --cpu=2 --gpu=1 --memory=10000 -- python3 -m semi_supervised.experiments.vat.cifar10_test
-wait
-echo "Finished"
